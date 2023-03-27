ab72b6bc8b5b3db2ad9e59a3367c928e76282c69
SimoneLollini <lollinisimone@gmail.com>
2023-03-27 17:43:18 +0200

    end exercise

--- src/main/java/com/lesson/bestoftheyear/controller/HomeController.java
+++ src/main/java/com/lesson/bestoftheyear/controller/HomeController.java
@@ -1,34 +1,72 @@
 package com.lesson.bestoftheyear.controller;
 
+import com.lesson.bestoftheyear.model.Movie;
+import com.lesson.bestoftheyear.model.Song;
 import com.lesson.bestoftheyear.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-//Step 1
-//Creare un nuovo progetto Spring Boot MVC + Thymeleaf utilizzando Spring Initializr. Il nome del progetto è best-of-the-year (stesso nome del repo).
-//Nel progetto aggiungere un controller che risponde alla root dell’applicazione,
-// con un metodo che restituisce una view fatta con Thymeleaf in cui viene stampato un titolo:
-// “Best of the year by …” (al posto dei puntini deve apparire il vostro nome,
-// passato come attributo dal controller attraverso il Model).
 
-//Step 2 Creare all’interno del controller due metodi privati : - uno restituisce una lista di oggetti di tipo Movie - getBestMovies() - l’altro restituisce una lista di oggetti di tipo Song - getBestSongs() Creare le classi Movie e Song aventi almeno :
-//- un id
-//- un titolo
-//Aggiungere al controller altri due metodi, che rispondono agli url - “/movies” - “/songs”
-//In ognuno di questi metodi aggiungere al Model un attributo stringa con una lista di titoli di migliori film o canzoni (in base al metodo che stiamo implementando) separati da virgole. Utilizzare i due metodi getBest… per recuperare i film e le canzoni.
-//Creare i rispettivi template Thymeleaf.
+import java.util.ArrayList;
+import java.util.List;
+
 //BONUS
 //Creare due metodi - “/movies/{id}” - “/songs/{id}” che dato il parametro id passato tramite il path, mostri in pagina il titolo relativo al film / canzone. Testare chiamando dal browser i diversi url.
 @Controller
 @RequestMapping("/")
 public class HomeController {
-    @GetMapping
-    public String home(Model model){
-        User newUser = new User("Simone");
-        model.addAttribute("username", newUser.getName());
-        return "home";
+@GetMapping
+        public String home(Model model){
+            User newUser = new User("Simone");
+            model.addAttribute("username", newUser.getName());
+            return "home";
     }
 
+   public List<Movie> getBestMovies(){
+       List<Movie> movies = new ArrayList<>();
+       for (int i = 1; i < 11; i++) {
+           Movie movie = new Movie(i, "Film " + i);
+           movies.add(movie);
+       }
+       return movies;
+   }
+
+    public List<Song> getBestSongs(){
+        List<Song> songs = new ArrayList<>();
+        for (int i = 1; i < 11; i++) {
+            Song song = new Song(i, "Song " + i);
+            songs.add(song);
+        }
+        return songs;
+    }
+
+
+    @RequestMapping("/movies")
+    public String movies(Model model){
+        List<Movie> movies = getBestMovies();
+        StringBuilder movieList  = new StringBuilder();
+
+        for (Movie movie: movies ) {
+            movieList.append(movie.getTitle()).append(", ");
+        }
+
+        model.addAttribute( "bestMovies", movieList.toString());
+        return "movies";
+    }
+
+    @RequestMapping("/songs")
+    public String songs(Model model)
+    {
+        List<Song> songs = getBestSongs();
+        StringBuilder songList  = new StringBuilder();
+
+        for (Song movie: songs ) {
+            songList.append(movie.getTitle()).append(", ");
+        }
+
+        model.addAttribute("bestSongs",songList.toString());
+        return "songs";
+    }
 }
