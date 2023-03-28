6f19cbe802f6d7df57034e8973c09e030d6dd92f
SimoneLollini <lollinisimone@gmail.com>
2023-03-28 14:57:00 +0200

    end step 1

--- src/main/java/com/lesson/bestoftheyear/controller/HomeController.java
+++ src/main/java/com/lesson/bestoftheyear/controller/HomeController.java
@@ -7,33 +7,42 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
 
+//Continuiamo a lavorare sul nostro progetto Best of the year (anche stessa repo).
+//Step 1
+//Modificare i metodi dei controller che rispondono agli url
+//- “/movies”
+//- “/songs”
+//in modo che i Model restituiscano una lista di oggetti (Movie o Song) invece di una stringa. Modificare anche le rispettive view per gestire la lista come unordered list.
+//Ogni elemento mostrato nella lista (film o canzone) deve essere un link che punta alla rispettiva pagina di dettaglio, in cui mostrare tutti i dati dell’oggetto e non solo il titolo.
+//Step 2
+//Includere Bootstrap e fare il refactoring del layout come da allegato, cercando di creare componenti riutilizzabili con i fragments.
+//In tutte le pagine deve essere mostrata una navbar con i link alla home, a movies e a songs.
 //BONUS
-//Creare due metodi - “/movies/{id}” - “/songs/{id}” che dato il parametro id passato tramite il path, mostri in pagina il titolo relativo al film / canzone. Testare chiamando dal browser i diversi url.
+//Evidenziare con la classe active il link della navbar corrispondente all’area che si sta navigando in quel momento
 @Controller
 @RequestMapping("/")
 public class HomeController {
-@GetMapping
-        public String home(Model model){
-            User newUser = new User("Simone");
-            model.addAttribute("username", newUser.getName());
-            return "home";
+    @GetMapping
+    public String home(Model model) {
+        User newUser = new User("Simone");
+        model.addAttribute("username", newUser.getName());
+        return "home";
     }
 
-   public List<Movie> getBestMovies(){
-       List<Movie> movies = new ArrayList<>();
-       for (int i = 1; i < 11; i++) {
-           Movie movie = new Movie(i, "Film " + i);
-           movies.add(movie);
-       }
-       return movies;
-   }
+    public List<Movie> getBestMovies() {
+        List<Movie> movies = new ArrayList<>();
+        for (int i = 1; i < 11; i++) {
+            Movie movie = new Movie(i, "Film " + i);
+            movies.add(movie);
+        }
+        return movies;
+    }
 
-    public List<Song> getBestSongs(){
+    public List<Song> getBestSongs() {
         List<Song> songs = new ArrayList<>();
         for (int i = 1; i < 11; i++) {
             Song song = new Song(i, "Song " + i);
@@ -44,29 +53,29 @@ public class HomeController {
 
 
     @RequestMapping("/movies")
-    public String movies(Model model){
+    public String movies(Model model) {
         List<Movie> movies = getBestMovies();
-        StringBuilder movieList  = new StringBuilder();
+//        StringBuilder movieList  = new StringBuilder();
+//
+//        for (Movie movie: movies ) {
+//            movieList.append(movie.getTitle()).append(", ");
+//        }
 
-        for (Movie movie: movies ) {
-            movieList.append(movie.getTitle()).append(", ");
-        }
-
-        model.addAttribute( "bestMovies", movieList.toString());
+        model.addAttribute("movieList", movies);
         return "movies";
     }
 
+
     @RequestMapping("/songs")
-    public String songs(Model model)
-    {
+    public String songs(Model model) {
         List<Song> songs = getBestSongs();
-        StringBuilder songList  = new StringBuilder();
+//        StringBuilder songList  = new StringBuilder();
+//
+//        for (Song movie: songs ) {
+//            songList.append(movie.getTitle()).append(", ");
+//        }
 
-        for (Song movie: songs ) {
-            songList.append(movie.getTitle()).append(", ");
-        }
-
-        model.addAttribute("bestSongs",songList.toString());
+        model.addAttribute("songList", songs);
         return "songs";
     }
 }
